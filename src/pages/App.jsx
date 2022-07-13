6992696b7f4030d2690358130c8aefd0d11e6005
Vinícius Soares <vsspaulasouza@gmail.com>
2022-07-13 17:19:00 -0300

    feat: added chackra ui

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Heading } from "@chakra-ui/react";
 
 function App() {
-  return <h1>Hello world!</h1>;
+    return <Heading>Hello world!</Heading>;
 }
 
 export default App;
