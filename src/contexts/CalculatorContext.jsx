e452b29bcd9ce881a9fed6cdfc47487127134407
Vinícius Soares <vsspaulasouza@gmail.com>
2022-08-22 17:31:52 -0300

    fix: eslint errors

--- src/contexts/CalculatorContext.jsx
+++ src/contexts/CalculatorContext.jsx
@@ -1,18 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export const CalculatorContext = React.createContext({});
 
-export const CalculatorContextProvider = ({ children }) => {
+export function CalculatorContextProvider({ children }) {
     const [number, setNumber] = React.useState("0");
     const [secondaryNumber, setSecondaryNumber] = React.useState("");
 
+    const value = React.useMemo(
+        () => ({ number, setNumber, secondaryNumber, setSecondaryNumber }),
+        [number, secondaryNumber]
+    );
+
     return (
-        <CalculatorContext.Provider
-            value={{ number, setNumber, secondaryNumber, setSecondaryNumber }}
-        >
+        <CalculatorContext.Provider value={value}>
             {children}
         </CalculatorContext.Provider>
     );
-};
+}
 
-export const useCalculator = React.useContext(CalculatorContext);
+export const useCalculator = () => React.useContext(CalculatorContext);
+
+CalculatorContextProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
