ca41599d672c79f157b477c9bea00c31df9107cf
Vinícius Soares <vsspaulasouza@gmail.com>
2022-08-23 17:33:31 -0300

    feat: adding operations

--- src/contexts/CalculatorContext.jsx
+++ src/contexts/CalculatorContext.jsx
@@ -3,6 +3,11 @@ import PropTypes from "prop-types";
 
 export const CalculatorContext = React.createContext({});
 
+function clear(currentNumber, currentSecondaryNumber, currentOperation) {
+    const secondaryNumber = currentNumber ? currentSecondaryNumber : "";
+    return { number: "", secondaryNumber, operation: currentOperation };
+}
+
 function changeOperator(value) {
     if (value === "") return value;
     return `${-value}`;
@@ -12,33 +17,65 @@ function addDot(value) {
     if (!value.includes(".")) return `${value}.`;
     return value;
 }
+
+function handleOperation(
+    currentNumber,
+    currentSecondaryNumber,
+    currentOperation,
+    newOperation
+) {
+    let number = currentNumber;
+    let secondaryNumber = currentSecondaryNumber;
+    let operation = currentOperation;
+    if (currentSecondaryNumber === "" && currentNumber !== "") {
+        number = "";
+        secondaryNumber = currentNumber + newOperation;
+        operation = newOperation;
+    }
+
+    return { number, secondaryNumber, operation };
+}
 export function CalculatorContextProvider({ children }) {
     const reducer = (state, action) => {
         switch (action.type) {
             case "clearNumber":
-                return { number: "", secondaryNumber: state.secondaryNumber };
+                return clear(
+                    state.number,
+                    state.secondaryNumber,
+                    state.operation
+                );
             case "addValue":
                 return {
                     number: state.number + action.value,
                     secondaryNumber: state.secondaryNumber,
+                    operation: state.operation,
                 };
             case "changeOperator":
                 return {
                     number: changeOperator(state.number),
                     secondaryNumber: state.secondaryNumber,
+                    operation: state.operation,
                 };
             case "addDot":
                 return {
                     number: addDot(state.number),
                     secondaryNumber: state.secondaryNumber,
+                    operation: state.operation,
                 };
+            case "operation":
+                return handleOperation(
+                    state.number,
+                    state.secondaryNumber,
+                    state.operation,
+                    action.value
+                );
             case "clearSecondaryNumber":
                 return { number: state.number, secondaryNumber: "" };
             default:
                 return state;
         }
     };
-    const initialState = { number: "", secondaryNumber: "" };
+    const initialState = { number: "", secondaryNumber: "", operation: "" };
 
     const [state, dispatch] = React.useReducer(reducer, initialState);
 
