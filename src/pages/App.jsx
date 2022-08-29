e3e94b8dc8f107ddaf92ae778f9470cddc2da30b
Vinícius Soares <vsspaulasouza@gmail.com>
2022-08-29 17:03:11 -0300

    fix: calculator export

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
 import "@fontsource/share-tech-mono";
-import Calculator from "../components/Calculator";
 import { CalculatorContextProvider } from "../contexts/CalculatorContext";
+import Calculator from "../components";
 
 function App() {
     return (
