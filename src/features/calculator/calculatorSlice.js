d3ad48cf9072f180172628fb41cc6451784da4b7
Vinícius Soares <vsspaulasouza@gmail.com>
2022-09-21 03:38:28 -0300

    fix: refactoring calculator slice

--- src/features/calculator/calculatorSlice.js
+++ src/features/calculator/calculatorSlice.js
@@ -1,113 +1,94 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const reducers = {
+    clear({ number, secondaryNumber, operation }) {
+        const newSecondaryNumber = number ? secondaryNumber : "";
+        return {
+            number: "",
+            secondaryNumber: newSecondaryNumber,
+            operation,
+        };
+    },
+    addValue({ number, secondaryNumber, operation }, { payload }) {
+        return {
+            number: number + payload,
+            secondaryNumber,
+            operation,
+        };
+    },
+    changeOperator({ number, secondaryNumber, operation }) {
+        const newNumber = number === "" ? number : `${-number}`;
+        return {
+            number: newNumber,
+            secondaryNumber,
+            operation,
+        };
+    },
+    addDot({ number, secondaryNumber, operation }) {
+        let newNumber = number;
+        if (!number.includes(".") && number !== "") newNumber = `${number}.`;
+        return {
+            number: newNumber,
+            secondaryNumber,
+            operation,
+        };
+    },
+    addOperation(currentState, { payload }) {
+        if (currentState.secondaryNumber === "" && currentState.number !== "") {
+            return {
+                number: "",
+                secondaryNumber: currentState.number + payload,
+                operation: payload,
+            };
+        }
+        return currentState;
+    },
+    solveResult({ number, secondaryNumber, operation }) {
+        const secondaryNumberFiltered = secondaryNumber.slice(0, -1);
+        let result = "";
+        switch (operation) {
+            case "%":
+                result = (secondaryNumberFiltered * number) / 100;
+                break;
+            case "÷":
+                result = secondaryNumberFiltered / number;
+                break;
+            case "x":
+                result = secondaryNumberFiltered * number;
+                break;
+            case "-":
+                result = secondaryNumberFiltered - number;
+                break;
+            case "+":
+                result = +secondaryNumberFiltered + +number;
+                break;
+            default:
+                break;
+        }
+
+        result = Number(result).toFixed(2);
+        if (result.endsWith(".00")) [result] = result.split(".");
+
+        return {
+            number: result.toString(),
+            secondaryNumber: "",
+            operation: "",
+        };
+    },
+    deleteCharacter({ number, secondaryNumber, operation }) {
+        const newNumber = number.slice(0, -1);
+        return {
+            number: newNumber,
+            secondaryNumber,
+            operation,
+        };
+    },
+};
+
 const slice = createSlice({
     name: "calculator",
     initialState: { number: "", secondaryNumber: "", operation: "" },
-    reducers: {
-        clear(currentState) {
-            const newSecondaryNumber = currentState.number
-                ? currentState.secondaryNumber
-                : "";
-            return {
-                number: "",
-                secondaryNumber: newSecondaryNumber,
-                operation: currentState.operation,
-            };
-        },
-        addValue(currentState, action) {
-            return {
-                number: currentState.number + action.payload,
-                secondaryNumber: currentState.secondaryNumber,
-                operation: currentState.operation,
-            };
-        },
-        changeOperator(currentState) {
-            const newNumber =
-                currentState.number === ""
-                    ? currentState.number
-                    : `${-currentState.number}`;
-            return {
-                number: newNumber,
-                secondaryNumber: currentState.secondaryNumber,
-                operation: currentState.operation,
-            };
-        },
-        addDot(currentState) {
-            let newNumber = currentState.number;
-            if (
-                !currentState.number.includes(".") &&
-                currentState.number !== ""
-            )
-                newNumber = `${currentState.number}.`;
-            return {
-                number: newNumber,
-                secondaryNumber: currentState.secondaryNumber,
-                operation: currentState.operation,
-            };
-        },
-        addOperation(currentState, action) {
-            let newNumber = currentState.number;
-            let newSecondaryNumber = currentState.secondaryNumber;
-            let newOperation = currentState.operation;
-            if (
-                currentState.secondaryNumber === "" &&
-                currentState.number !== ""
-            ) {
-                newNumber = "";
-                newSecondaryNumber = currentState.number + action.payload;
-                newOperation = action.payload;
-            }
-            return {
-                number: newNumber,
-                secondaryNumber: newSecondaryNumber,
-                operation: newOperation,
-            };
-        },
-        solveResult(currentState) {
-            const secondaryNumberFiltered = currentState.secondaryNumber.slice(
-                0,
-                -1
-            );
-            let result = "";
-            switch (currentState.operation) {
-                case "%":
-                    result =
-                        (secondaryNumberFiltered * currentState.number) / 100;
-                    break;
-                case "÷":
-                    result = secondaryNumberFiltered / currentState.number;
-                    break;
-                case "x":
-                    result = secondaryNumberFiltered * currentState.number;
-                    break;
-                case "-":
-                    result = secondaryNumberFiltered - currentState.number;
-                    break;
-                case "+":
-                    result = +secondaryNumberFiltered + +currentState.number;
-                    break;
-                default:
-                    break;
-            }
-
-            result = Number(result).toFixed(2);
-            if (result.endsWith(".00")) [result] = result.split(".");
-
-            return {
-                number: result.toString(),
-                secondaryNumber: "",
-                operation: "",
-            };
-        },
-        deleteCharacter(currentState) {
-            const newNumber = currentState.number.slice(0, -1);
-            return {
-                number: newNumber,
-                secondaryNumber: currentState.secondaryNumber,
-                operation: currentState.operation,
-            };
-        },
-    },
+    reducers,
 });
 
 export const {
