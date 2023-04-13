ca74ef991fc73b8c01484ef177e60a3ee7d73288
Vinícius Soares <vsspaulasouza@gmail.com>
2023-04-13 02:58:07 -0300

    adding pwa files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
+const manifestForPlugin = {
+    registerType: "prompt",
+    includeAssets: ["public/icon.png"],
+    manifest: {
+        name: "Calculadora",
+        short_name: "Calculadora",
+        icons: [
+            {
+                src: "/public/icon.png",
+                sizes: "1299x1299",
+                type: "image/png",
+                purpose: "any maskable",
+            },
+        ],
+        start_url: "/",
+        scope: "/",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+        orientation: "portrait",
+    },
+};
+
 export default defineConfig({
-  plugins: [react()]
-})
+    plugins: [react(), VitePWA(manifestForPlugin)],
+});
