8c92311b827e12e195ac431dc78e8934c6e52fd7
Vinícius Soares <vsspaulasouza@gmail.com>
2023-04-13 03:17:43 -0300

    updating cfg

--- vite.config.js
+++ vite.config.js
@@ -2,32 +2,28 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-const manifestForPlugin = {
-    registerType: "prompt",
-    workbox: {
-        globPatterns: ["**/*"],
-    },
-    includeAssets: ["**/*"],
-    manifest: {
-        name: "Calculadora",
-        short_name: "Calculadora",
-        icons: [
-            {
-                src: "/public/icon.png",
-                sizes: "1299x1299",
-                type: "image/png",
-                purpose: "any maskable",
-            },
-        ],
-        start_url: "/",
-        scope: "/",
-        display: "standalone",
-        theme_color: "#000000",
-        background_color: "#ffffff",
-        orientation: "portrait",
-    },
-};
-
 export default defineConfig({
-    plugins: [react(), VitePWA(manifestForPlugin)],
+    plugins: [
+        react(),
+        VitePWA({
+            manifest: {
+                name: "Calculadora",
+                short_name: "Calculadora",
+                start_url: "/",
+                display: "standalone",
+                background_color: "#ffffff",
+                lang: "en",
+                scope: "/",
+                icons: [
+                    {
+                        src: "/icon.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any maskable",
+                    },
+                ],
+                theme_color: "#ffffff",
+            },
+        }),
+    ],
 });
