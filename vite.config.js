445632fa873e9c83f4294b7667c7b4615bcc2fc0
Vinícius Soares <vsspaulasouza@gmail.com>
2023-04-13 03:05:45 -0300

    adding config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import { VitePWA } from "vite-plugin-pwa";
 
 const manifestForPlugin = {
     registerType: "prompt",
-    includeAssets: ["public/icon.png"],
+    workbox: {
+        globPatterns: ["**/*"],
+    },
+    includeAssets: ["**/*"],
     manifest: {
         name: "Calculadora",
         short_name: "Calculadora",
